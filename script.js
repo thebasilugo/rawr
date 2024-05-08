b12910cf3d34780c0ddeb1fd3c22462d2a3c650e
Basil, Ugo <115357283+thebasilugo@users.noreply.github.com>
2024-05-08 12:02:34 +0100

    pranks suck

--- script.js
+++ script.js
@@ -73,38 +73,64 @@
 // setTimeout(showLastPart, 45000);
 
 
-// deactivated section
+// // deactivated section
 
-function deactivatedh3(){
-  var typingEffect = new Typed(".deactiv-h3", {
-    strings : [
-      "d3",
-      "dea",
-      "deaC",
-      "deacti",
-      "deactiv@",
-      "deactiva",
-      "deactivat3D",
-      "deactivated",
-      "deactivated.",
-      ],
-    loop : false,
-    typeSpeed : 50,
-    backSpeed : 30,
-    backDelay : 10,
-  })
+// function deactivatedh3(){
+//   var typingEffect = new Typed(".deactiv-h3", {
+//     strings : [
+//       "d3",
+//       "dea",
+//       "deaC",
+//       "deacti",
+//       "deactiv@",
+//       "deactiva",
+//       "deactivat3D",
+//       "deactivated",
+//       "deactivated.",
+//       ],
+//     loop : false,
+//     typeSpeed : 50,
+//     backSpeed : 30,
+//     backDelay : 10,
+//   })
+// }
+
+// function deactivatedp(){
+//   var typingEffect = new Typed(".deactiv-p", {
+//     strings : [
+//        "this page has been deactivated<br /><br /><br /><br /><br /><em>...although my love for her hasn't, and never will</em> ;)",
+//       ],
+//     loop : false,
+//     typeSpeed : 90,
+//     backSpeed : 80,
+//     backDelay : 1000,
+//   })
+// }
+
+
+// i messed up and played a sick joke on her
+function deactivatedh3(){var typingEffect = new Typed(".deactiv-h3", {
+  strings : [
+    "to: her",
+    ],
+  loop : false,
+  typeSpeed : 50,
+  backSpeed : 30,
+  backDelay : 10,
+})
 }
 
 function deactivatedp(){
-  var typingEffect = new Typed(".deactiv-p", {
-    strings : [
-       "this page has been deactivated<br /><br /><br /><br /><br /><em>...although my love for her hasn't, and never will</em> ;)",
-      ],
-    loop : false,
-    typeSpeed : 90,
-    backSpeed : 80,
-    backDelay : 1000,
-  })
+var typingEffect = new Typed(".deactiv-p", {
+  strings : [
+     "i hope you come here when my words aren't enough, and when my lips have all but the right things to say <br/><br/>",
+     "i hope you come here when my words aren't enough, and when my lips have all but the right things to say <br/><br/>my phone would’ve been dead for hours, thanks to the unstable power supply.<br/> and i think i might be grateful for it<br/>at least this way, i don’t show [her] how distrustful i am<br/>i said i was going away for a long time and i want to<br/>i want to mean it—to go. to disappear.<br/>i want to hurt, and i do<br/>i want to pick up another phone and put a call across, but i don’t know what’s stopping me—<br/>is it the words i said when i couldn’t think clearly?<br/>or perhaps, the feeling that more words from me would cause even more damage?<br/>or worse still, maybe it’s the understanding that my words have lost their potency and value.<br/>—and i get it. not really, but i do.<br/>how did i let it get this way? how did i let it get this far?<br/>the awful jokes? the sick prank? the love? the obsession?<br/>it’s overwhelming every part of me<br/>i see her when i close my eyes<br/>i smell her when i inhale<br/>i taste her—my mind’s conception of her—on my tongue, with every swallow and with every gulp that goes down my throat.<br/>i lose myself with every breath, and with every exhale out of my mouth and nostrils.<br/>i feel lost, and i feel like i’ve lost, and i hate the feeling.<br/>it’s been beautiful—every part of it, but does it have to end?<br/>i marvel at cataclysm—messing things up instantaneously and i don’t think it surprises me anymore, but it still hurts—now more than ever, <br/>and i wouldn’t wish this hurt on anyone<br/>it’s a lot more than pain. <br/>it’s heartbreak. <br/>it’s anger. <br/>it’s guilt. <br/>but somehow i hope to find hope—that it’s not the end.<br/>i hope that it’s not the end.<br/>i hope it, with everything in me.<br/>i hope she sees me when she closes her eyes.<br/>i hope she smells me when she inhales.<br/>i hope she tastes me on her tongue and with every swallow, and with every gulp <br/>i hope she remembers—<br/>not this sucky version that she distrusts so much,<br/>but the earlier vanilla version that she adored,<br/>the version she was a huge fan of,<br/>the version that never hurt her, that was always there, that never hungered for a desperate response.<br/>the version that always tried to make everything right.<br/>i hope she remembers the version that she fell in love with, and i hope she chooses him again.<br/>i hope she chooses me again<br/>i hope you choose me all over again, and even if that takes an even longer time, i hope you fall in love with me again.<br/><br/>i hope the hatred dissolves, and the anger dissipates,<br/>maybe then will my eyes close again, and i’ll see the familiar eigengrau that holds very little in my memory.<br/>maybe then will i inhale and it would be oxygen that floods my lungs.<br/>maybe then will my tongue taste the salt and spice in that which i eat, or the sweet in that which i drink, or maybe even the very saliva that has been it’s companion for as long as it remembers.<br/>maybe then will the hurt go away and the pain cease, <br/>and i’d be reunited with my best friend, and sister,<br/>my lover, and biggest fan,<br/>my little weirdo,<br/>for without her, the werewolf loses his pack, and is forced to roam the world alone—although not for long, <br/>for the lone wolf dies but the pack survives.",
+    ],
+  loop : false,
+  typeSpeed : 90,
+  backSpeed : 80,
+  backDelay : 1000,
+})
 }
 
 deactivatedh3();
